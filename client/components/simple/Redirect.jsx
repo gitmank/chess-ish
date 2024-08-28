300ae690066f9b4b3450a663cab22bee216a3af8
Manomay Kagalkar <manomay111@gmail.com>
2024-08-28 23:50:17 +0530

    Add redirect pages for unauthenticated users

--- client/components/simple/Redirect.jsx
+++ client/components/simple/Redirect.jsx
@@ -1,13 +1,13 @@
-export default function Redirect({ page = "" }) {
+export default function Redirect({ to = "/" }) {
     return (
         <main className="flex min-h-screen flex-col items-center justify-center space-y-12 p-12 text-center">
             <h1 className="text-4xl font-bold">👋 Hiee</h1>
-            <p className="text-xl">You are being redirected to /{page}</p>
+            <p className="text-xl">You are being redirected to {to}</p>
             <a
-                href={`/${page || ""}`}
+                href={`${to}`}
                 className="btn rounded-full p-1 px-2 bg-blue-400 hover:bg-blue-500 duration-75"
             >
-                Go to {page || "Home"}
+                Go to {to === "/" ? "Home" : to}
             </a>
         </main>
     );
